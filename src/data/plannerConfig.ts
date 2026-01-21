2e166d17ce0f17387843d86e64092e476e359cbb
Tiangong777 <1002497954@qq.com>
2026-01-21 16:35:56 +0800

    更新了配置

--- src/data/plannerConfig.ts
+++ src/data/plannerConfig.ts
@@ -23,5 +23,93 @@ export interface PlannerConfig {
 }
 
 export const sharedConfig: Partial<PlannerConfig> = {
-    // Current placeholder, will be filled by full export
+    "title": "年会筹备",
+    "time": "2月6日 下午六点开始",
+    "location": "龙山露营地 途居厅",
+    "budget": 27865,
+    "floatRatio": 0,
+    "peopleDigitalCenter": 68,
+    "peopleCommercialVehicle": 54,
+    "itbpLeads": [
+        { "name": "牛志高", "people": 0, "perPerson": 0, "extra": 0 },
+        { "name": "汪亚青", "people": 0, "perPerson": 0, "extra": 0 },
+        { "name": "陆旭宴", "people": 1, "perPerson": 0, "extra": 0 },
+        { "name": "李昊", "people": 2, "perPerson": 212, "extra": 0 },
+        { "name": "左晓海", "people": 0, "perPerson": 0, "extra": 0 },
+        { "name": "张主涛", "people": 0, "perPerson": 0, "extra": 0 },
+        { "name": "李云峰", "people": 0, "perPerson": 0, "extra": 0 }
+    ],
+    "peopleGuests": 10,
+    "tableCount": 14,
+    "mealPrice": 1000,
+    "dinnerMenu": [
+        { "title": "冷菜", "items": ["精美四冷"] },
+        {
+            "title": "热菜",
+            "items": [
+                "美极鲜白条鱼", "白灼南美大虾", "地锅跑山鸡", "金汤羊蝎子",
+                "徽州腊鲜笋", "黄标牛肉", "外婆蒸土鸡蛋", "山药木耳",
+                "蒿子咸肉丝", "高山有机青菜"
+            ]
+        },
+        { "title": "汤类", "items": ["酸萝卜老鸭汤（切块）"] },
+        { "title": "甜品", "items": ["红糖糍粑", "精美果盘", "饮料2瓶"] }
+    ],
+    "cart": {
+        "horse-creativity": 28,
+        "cat-blindbox": 28,
+        "material-red-envelope-packaging": 180,
+        "material-tabletop-football": 2,
+        "custom-material-1768378074197": 1,
+        "prize-grand": 1,
+        "prize-1": 2,
+        "prize-2": 7,
+        "prize-3": 5,
+        "gujing-8-500": 28,
+        "snow-qingshuang-bottle": 24,
+        "red-5": 110,
+        "red-10": 40,
+        "red-20": 20,
+        "red-50": 3,
+        "red-100": 2,
+        "custom-red-envelope-1768748845499": 1,
+        "custom-red-envelope-1768748892512": 2,
+        "custom-red-envelope-1768748870031": 1,
+        "custom-red-envelope-1768748955405": 3,
+        "custom-prize-1768791547078": 14
+    },
+    "customMaterials": [
+        {
+            "id": "custom-material-1768378074197",
+            "category": "Materials",
+            "name": "横幅",
+            "unit": "个",
+            "spec": "自定义",
+            "price": 35
+        }
+    ],
+    "customPrizes": [
+        {
+            "id": "custom-prize-1768791547078",
+            "category": "Prizes",
+            "name": "唱歌马年文创",
+            "unit": "?",
+            "spec": "1",
+            "price": 40
+        }
+    ],
+    "prizeEdits": {
+        "prize-grand": { "name": "特等奖", "spec": "大奖", "unit": "个", "price": 800 },
+        "prize-1": { "name": "一等奖", "spec": "大奖", "unit": "个", "price": 500 },
+        "prize-2": { "name": "二等奖", "spec": "大奖", "unit": "个", "price": 150 },
+        "prize-3": { "name": "三等奖 导演小组抽", "spec": "大奖", "unit": "个", "price": 50 }
+    },
+    "customRedEnvelopes": [
+        { "id": "custom-red-envelope-1768748845499", "category": "RedEnvelope", "name": "足球冠军红包", "unit": "1", "spec": "现金", "price": 800 },
+        { "id": "custom-red-envelope-1768748870031", "category": "RedEnvelope", "name": "足球亚军红包", "unit": "个", "spec": "现金", "price": 400 },
+        { "id": "custom-red-envelope-1768748892512", "category": "RedEnvelope", "name": "三四名红包", "unit": "1", "spec": "现金", "price": 200 },
+        { "id": "custom-red-envelope-1768748955405", "category": "RedEnvelope", "name": "剩下三名红包", "unit": "1", "spec": "现金", "price": 100 }
+    ],
+    "redEnvelopeEdits": {},
+    "itemEdits": {}
 };
